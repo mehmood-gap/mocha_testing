9ff37dabf04e9a4b69515d8e17ddad9a966b06db
Mehmood Munir <80746663+mehmood-gap@users.noreply.github.com>
2021-08-26 16:49:29 +0500

    files

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import { Test } from 'mocha';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+  
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+      
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
